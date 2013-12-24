2f1722e9958daaa1e5d97ad25a0280ef99298038
sh1r0 <sh1r0@github.fake>
2013-12-24 16:20:31 +0800

    fix range bug & add constraint for pants

--- single-item-roi/extract_roi.cpp
+++ single-item-roi/extract_roi.cpp
@@ -40,7 +40,7 @@ int main(int argc, char const *argv[])
 	Mat im_gray;
 	cvtColor(im_rgb, im_gray, CV_RGB2GRAY);
 
-	Mat im_bw = im_gray < 253;
+	Mat im_bw = im_gray < 254;
 	medianBlur(im_bw, im_bw, 5);
 
 	unsigned char *mask = im_bw.data;
@@ -49,7 +49,7 @@ int main(int argc, char const *argv[])
 
 	int *dp_table = new int[height*width];
 	for (int i = 0; i < width; ++i) {
-		dp_table[i] = mask[i];
+		dp_table[i] = mask[i] > 0;
 		for (int j = 1; j < height; j++) {
 			dp_table[j*width+i] = (mask[j*width+i]) ? dp_table[(j-1)*width+i]+1 : 0;
 		}
@@ -74,15 +74,16 @@ int main(int argc, char const *argv[])
 			} else {
 				while (h < rect_stack.top().height) {
 					left = rect_stack.top();
-					int w = j - left.position;
-					int area = w*left.height;
+					int _w = j - left.position;
+					int _h = left.height;
+					int area = _w*_h;
 
-					if (area > max_area) {
+					if (area > max_area && left.position < (width>>1) && (_w > (width>>1) || (_h > (height>>1)))) {
 						max_area = area;
 						max_x = left.position;
-						max_y = i - left.height + 1;
-						max_w = w;
-						max_h = left.height;
+						max_y = i - _h + 1;
+						max_w = _w;
+						max_h = _h;
 					}
 
 					rect_stack.pop();
@@ -101,34 +102,45 @@ int main(int argc, char const *argv[])
 
 		while (!rect_stack.empty()) {
 			left = rect_stack.top();
-			int w = width - left.position;
-			int area = w*left.height;
-			if (area > max_area) {
+			int _w = width - left.position;
+			int _h = left.height;
+			int area = _w*_h;
+
+			if (area > max_area && left.position < (width>>1) && (_w > (width>>1) || (_h > (height>>1)))) {
 				max_area = area;
 				max_x = left.position;
-				max_y = i - left.height + 1;
-				max_w = w;
-				max_h = left.height;
+				max_y = i - _h + 1;
+				max_w = _w;
+				max_h = _h;
 			}
+
 			rect_stack.pop();
 		}
 	}
 
-	int center_x = max_x + (max_w >> 1);
-	int center_y = max_y + (max_h >> 1);
-	int radius = (int)(max_w * 0.4);
-	Rect roi = Rect(center_x - radius, center_y - radius, 2*radius, 2*radius);
+	int x, y, h, w;
+	if (max_w*3 > max_h) {
+		w = 0.8 * min(max_w, max_h);
+		h = w;
+		x = max_x + 0.5 * max_w - 0.5 * w;
+		y = max_y + 0.5 * max_h - 0.5 * h;
+	} else {
+		x = max_x + 0.05 * max_w;
+		y = max_y + 0.1 * max_h;
+		w = 0.9 * max_w;
+		h = 0.4 * max_h;
+	}
+	Rect roi = Rect(x, y, w, h);
 	imwrite(output_prefix+"_patch.jpg", im_rgb(roi));
-/*
+
 	// show images for parameter tuning
-	imshow("roi", im_rgb(roi));
-	rectangle(im_rgb, Point(center_x - radius, center_y - radius),
-		Point(center_x + radius, center_y + radius), CV_RGB(0, 255, 0), 1, 8, 0);
-	rectangle(im_rgb, Point(max_x, max_y), Point(max_x+max_w-1, max_y+max_h-1),
-		CV_RGB(255, 0, 0), 1, 8, 0);
-	imshow("img", im_rgb);
-	waitKey();
+	// imshow("roi", im_rgb(roi));
+	rectangle(im_rgb, Point(x, y), Point(x + w - 1, y + h - 1), CV_RGB(0, 255, 0), 1, 8, 0);
+	rectangle(im_rgb, Point(max_x, max_y), Point(max_x+max_w-1, max_y+max_h-1), CV_RGB(255, 0, 0), 1, 8, 0);
+	// imshow("img", im_rgb);
+	// waitKey();
 	imwrite(output_prefix+"_roi.jpg", im_rgb);
-*/
+	// imwrite(output_prefix+"_mask.jpg", im_bw);
+
 	return 0;
 }
