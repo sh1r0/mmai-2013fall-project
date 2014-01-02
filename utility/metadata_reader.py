62a68aac7dbacf4675f71bab38ffcb2b81533dcd
jimmypunk <wang.jimmy319@gmail.com>
2014-01-02 17:16:50 +0800

    fix bug in metadata_reader; change the constructor

--- utility/metadata_reader.py
+++ utility/metadata_reader.py
@@ -2,32 +2,55 @@
 import json
 import sys
 import re
+import glob
 
 lower_part_category = ["Trousers","Shorts", "Jeans"]
 
 collar_opt = ['rollneck', 'v neck','zip collar', 'crew neck','hoodie','shirt-collar']
-pattern_opt = ['printed', 'striped', 'plain', 'checked']
+pattern_opt = ['printed', 'striped', 'plain', 'check']
 sleeve_opt = ['long sleeve', 'short sleeve', 'vest']
 material_opt = ['cotton', 'denim', 'leather','cashmere','poly']
-class metadata_reader():
-	metadata = ""
-	attribute = {
-		'clothing_category':None,
-		'collar':None,
-		'sleeve_len':None,
-		'pattern':None,
-		'button':None,
-		'material':[]
-	}
-	def __init__(self,path):
-		f = open(path)
+
+
+
+class metadata_reader(object):
+
+
+	def __init__(self, product_code, metadata_dir_path):
+		self.attribute = {
+			'clothing_category':None,
+			'collar':None,
+			'sleeve_len':None,
+			'pattern':None,
+			'button':None,
+			'material':[]
+		}
+		path_pattern = "%s/*/%s.txt"%(metadata_dir_path, product_code)
+		ret_path = glob.glob(path_pattern)
+		assert(len(ret_path)>0)
+		f = open(ret_path[0])
 		json_str = f.read()
 		self.metadata = json.loads(json_str)
+		self.metadata_dir_path = metadata_dir_path
+		print self.metadata['pair_item']
+		self.metadata['pair_item'] = self._trim_non_exist_pair_item(self.metadata['pair_item'])
+		print "trimmed_pair_item", self.metadata['pair_item']
 		self._fill_attr()
 
+	def _trim_non_exist_pair_item(self, pair_item_list):
+		trimmed_pair_item_list = []
+		for item in pair_item_list:
+			item_path = self.metadata_dir_path + '/*/%s.txt'%item
+			if len(glob.glob(item_path)) > 0:
+				trimmed_pair_item_list.append(item)
+		return trimmed_pair_item_list
+
+	def get_attr(self):
+		return self.attribute
+
 	def _fill_attr(self):
+		
 		self.attribute['clothing_category'] = self.metadata['clothing_category']
-		print self.metadata['pageClass']
 		
 		if self.metadata['clothing_category'] in lower_part_category:
 			if self.metadata['clothing_category']=='Shorts':
@@ -50,8 +73,11 @@ class metadata_reader():
 		elif 'Shirts' in self.metadata['clothing_category']:
 			self.attribute['button'] = 'yes'
 			self.attribute['collar'] = 'shirt-collar'
+			
 		elif self.metadata['clothing_category']=='Polos':
 			self.attribute['collar'] = 'shirt-collar'
+			
+
 								
 
 		for opt in sleeve_opt:
@@ -68,12 +94,9 @@ class metadata_reader():
 				break
 		for opt in material_opt:
 			if self.is_str_in_field(opt,['product_name','detail_care']):
-				self.attribute['material'].append(opt)
+				if opt not in self.attribute['material']:
+					self.attribute['material'].append(opt)
 
-		
-		
-
-		print self.attribute
 
 	def is_str_in_field(self, keyword, keys):
 		keyword = self.normalized_string(keyword)
@@ -81,7 +104,7 @@ class metadata_reader():
 			desc = self.normalized_string(self.metadata[key])
 			
 			if keyword in desc:
-				print keyword, desc
+				#print keyword, desc
 				return True
 		return False
 	def normalized_string(self,input_str):
@@ -91,5 +114,8 @@ class metadata_reader():
 
 
 
-print sys.argv[1]
-metadata_reader(sys.argv[1])
+if __name__ == "__main__":
+	assert(len(sys.argv) == 3)
+	print sys.argv[1]
+	#metadata_reader(product_code, metadata_dir)
+	metadata_reader(sys.argv[1], sys.argv[2])
