bb1caedc2ebf66db7510ea4e932fa5ac0730eb28
sh1r0 <sh1r0@github.fake>
2014-01-04 21:32:57 +0800

    fix wrong variable names

--- clothing-analysis/segment_upper_part_ROI.m
+++ clothing-analysis/segment_upper_part_ROI.m
@@ -5,17 +5,17 @@ function segment_upper_part_ROI(img_path)
     BW_im = (imgray<253);
     seD = strel('diamond',1);
     BWfinal = imerode(BW_im,seD);
-    imshow(BWfinal);
-    [w,h] = size(BWfinal);
-    roi_width = zeros(1,h);
-    roi_margin_left = zeros(1, h);
-    for i = 1:h
+    figure, imshow(BWfinal);
+    [h,w] = size(BWfinal);
+    roi_width = zeros(1,w);
+    roi_margin_left = zeros(1, w);
+    for i = 1:w
         roi_width(i) = sum(BWfinal(i,:));
 
         idx_list = find(BWfinal(i,:)==1);
         roi_margin_left(i) = idx_list(1);
     end
-    bar(roi_width)
+    figure, bar(roi_width);
     collar_region = find_collar_region(roi_width, BWfinal)
     sleeve_region = find_sleeve_region(roi_width, roi_margin_left, BWfinal)
 
@@ -24,13 +24,13 @@ function segment_upper_part_ROI(img_path)
     figure, imshow( im(collar_region.h_start:collar_region.h_end,collar_region.w_start:collar_region.w_end,:) )
     figure, imshow( im(sleeve_region.h_start:sleeve_region.h_end,sleeve_region.w_start:sleeve_region.w_end,:) )
 
-    
+
 end
 
 function sleeve_region = find_sleeve_region(roi_width, roi_margin_left, BWfinal)
     sleeve_threshold = quantile(roi_width, .7)
     idx_list = find(roi_width >= sleeve_threshold)
-    
+
     sleeve_region.h_start = idx_list(1)
     sleeve_region.h_end = idx_list(end)
 
@@ -43,7 +43,7 @@ end
 
 % find collar region
 function collar_region = find_collar_region(roi_width, BWfinal)
-    
+
     idx_list = find(roi_width == min(roi_width))
 
     collar_region.h_start = idx_list(end)
@@ -54,6 +54,6 @@ function collar_region = find_collar_region(roi_width, BWfinal)
 
     idx_list1 = find(BWfinal(collar_region.h_start,:)==1)
     idx_list2 = find(BWfinal(collar_region.h_end,:)==1)
-    collar_region.w_start = (idx_list1(1) + idx_list2(1))/2 
-    collar_region.w_end = (idx_list1(end) + idx_list2(end))/2     
+    collar_region.w_start = (idx_list1(1) + idx_list2(1))/2
+    collar_region.w_end = (idx_list1(end) + idx_list2(end))/2
 end
