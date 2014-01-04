4625e3f75be7c286a47d9ffce2d4a76c20592b17
jimmypunk <wang.jimmy319@gmail.com>
2014-01-05 03:17:21 +0800

    resolve conflict but bug unfixed yet

--- clothing-analysis/segment_upper_part_ROI.m
+++ clothing-analysis/segment_upper_part_ROI.m
@@ -1,4 +1,4 @@
-function segment_upper_part_ROI(img_path)
+function [sleeve_region_im, collar_region_im] = segment_upper_part_ROI(img_path)
 
     im=imread(img_path);
     imgray = rgb2gray(im);
@@ -21,39 +21,42 @@ function segment_upper_part_ROI(img_path)
 
     %sleeve_region
     %main_region
-    figure, imshow( im(collar_region.h_start:collar_region.h_end,collar_region.w_start:collar_region.w_end,:) )
-    figure, imshow( im(sleeve_region.h_start:sleeve_region.h_end,sleeve_region.w_start:sleeve_region.w_end,:) )
+    figure, imshow( im(collar_region.h_start:collar_region.h_end,collar_region.w_start:collar_region.w_end,:) );
+    figure, imshow( im(sleeve_region.h_start:sleeve_region.h_end,sleeve_region.w_start:sleeve_region.w_end,:) );
+    %figure, imshow( im(main_region.h_start:main_region.h_end,main_region.w_start:main_region.w_end,:) );
+    sleeve_region_im = im(sleeve_region.h_start:sleeve_region.h_end,sleeve_region.w_start:sleeve_region.w_end,:);
+    collar_region_im = im(collar_region.h_start:collar_region.h_end,collar_region.w_start:collar_region.w_end,:);
 
 
 end
 
 function sleeve_region = find_sleeve_region(roi_width, roi_margin_left, BWfinal)
-    sleeve_threshold = quantile(roi_width, .7)
-    idx_list = find(roi_width >= sleeve_threshold)
-
-    sleeve_region.h_start = idx_list(1)
-    sleeve_region.h_end = idx_list(end)
+    sleeve_threshold = quantile(roi_width, .7);
+    idx_list = find(roi_width >= sleeve_threshold);
+    
+    sleeve_region.h_start = idx_list(1);
+    sleeve_region.h_end = idx_list(end);
 
     %idx_list = find(roi_width == max(roi_width))
     %h_widest = idx_list(1)
-    sleeve_region.w_start = min(roi_margin_left)
-    sleeve_region.w_end = sleeve_region.w_start + 130
+    sleeve_region.w_start = min(roi_margin_left);
+    sleeve_region.w_end = sleeve_region.w_start + 130;
 end
 
 
 % find collar region
 function collar_region = find_collar_region(roi_width, BWfinal)
+    
+    idx_list = find(roi_width == min(roi_width));
 
-    idx_list = find(roi_width == min(roi_width))
+    collar_region.h_start = idx_list(end);
 
-    collar_region.h_start = idx_list(end)
+    threshold = quantile(roi_width, .3);
+    idx_list = find(roi_width >= threshold);
+    collar_region.h_end = idx_list(1);
 
-    threshold = quantile(roi_width, .3)
-    idx_list = find(roi_width >= threshold)
-    collar_region.h_end = idx_list(1)
-
-    idx_list1 = find(BWfinal(collar_region.h_start,:)==1)
-    idx_list2 = find(BWfinal(collar_region.h_end,:)==1)
-    collar_region.w_start = (idx_list1(1) + idx_list2(1))/2
-    collar_region.w_end = (idx_list1(end) + idx_list2(end))/2
+    idx_list1 = find(BWfinal(collar_region.h_start,:)==1);
+    idx_list2 = find(BWfinal(collar_region.h_end,:)==1);
+    collar_region.w_start = (idx_list1(1) + idx_list2(1))/2;
+    collar_region.w_end = (idx_list1(end) + idx_list2(end))/2;     
 end
